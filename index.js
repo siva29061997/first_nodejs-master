c097f17775ea3f93d6ee13be76b7a25684602d4e
siva29061997 <siva29061997@gmail.com>
2022-09-21 18:00:08 +0530

    NEW

--- index.js
+++ index.js
@@ -14,162 +14,213 @@ app.use(cors({
     origin: "http://localhost:3001"
 }));
 
-app.get("/users",async function (req, res) {
+app.get("/users", async function (req, res) {
 
     try {
-    // step 1:
-    const connection = await mongoClient.connect(URL)
-    // step 2:
-    const db = connection.db(DB);
-    // step 3:
-    // step 4:
-    let reUser = await db.collection("products").find().toArray();
-    // step 5:
-    await connection.close();
-    // error :
-    res.json(reUser);
+        // step 1:
+        const connection = await mongoClient.connect(URL)
+        // step 2:
+        const db = connection.db(DB);
+        // step 3:
+        // step 4:
+        let reUser = await db.collection("users").find().toArray();
+        // step 5:
+        await connection.close();
+        // error :
+        res.json(reUser);
     } catch (error) {
         res.status(500).json({
-            messege:"Somthig went wrong"
+            messege: "Somthig went wrong"
         });
     }
-    // let qParms = req.query
-    // console.log(qParms)
-
-    // let resUser = []
-
-    // for (let index = parseInt(req.query.offset); 
-    // index < parseInt(req.query.offset) + parseInt(req.query.limit); index++) {
-    //     if(users[index]){
-    //         resUser.push(users[index])
-    //     }
-    // }
 });
+app.get("/products", async function (req, res) {
 
-app.post("/user",async function (req, res) {
-    
     try {
-        
-    const connection = await mongoClient.connect(URL)
-    
-    const db = connection.db(DB)
-    
-    await db.collection("products").insertOne(req.body)
-    
-    await connection.close()
-   
-    res.status(200).json({messege:"Done"})
+        // step 1:
+        const connection = await mongoClient.connect(URL)
+        // step 2:
+        const db = connection.db(DB);
+        // step 3:
+        // step 4:
+        let reUser = await db.collection("products").find().toArray();
+        // step 5:
+        await connection.close();
+        // error :
+        res.json(reUser);
     } catch (error) {
         res.status(500).json({
-            messege:"Somthig went wrong"
+            messege: "Somthig went wrong"
+        });
+    }
+
+});
+
+app.post("/user", async function (req, res) {
+
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        await db.collection("users").insertOne(req.body)
+
+        await connection.close()
+
+        res.status(200).json({ messege: "Done" })
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        })
+    }
+});
+app.post("/product", async function (req, res) {
+
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        await db.collection("products").insertOne(req.body)
+
+        await connection.close()
+
+        res.status(200).json({ messege: "Done" })
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
         })
     }
 
-    // req.body.id = users.length + 1;
-    // users.push(req.body)
-    // res.json({ messege: "user created" });
 });
 
-app.get("/user/:id",async function (req, res) {
+app.get("/user/:id", async function (req, res) {
 
     try {
-        
-    const connection = await mongoClient.connect(URL)
-    
-    const db = connection.db(DB)
-    
-    let user = await db.collection("products").findOne({_id: mongodb.ObjectId(req.params.id)});
-   
-    await connection.close()
-    
-    res.status(200).json(user)
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let user = await db.collection("users").findOne({ _id: mongodb.ObjectId(req.params.id) });
+
+        await connection.close()
+
+        res.status(200).json(user)
     } catch (error) {
         res.status(500).json({
-            messege:"Somthig went wrong"
+            messege: "Somthig went wrong"
+        })
+    }
+});
+app.get("/product/:id", async function (req, res) {
+
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let user = await db.collection("products").findOne({ _id: mongodb.ObjectId(req.params.id) });
+
+        await connection.close()
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
         })
     }
 
-    // let userId = req.params.id;
-    // let user = users.find((item) => item.id == userId);
-    // if (user) {
-    //     res.json(user)
-    // } else {
-    //     res.json({ messege: "User not found" });
-    // }
-})
-
-app.put("/user/:id",async function (req, res) {
-
-    try {
-        
-    const connection = await mongoClient.connect(URL)
-   
-    const db = connection.db(DB)
-    
-    let user = await db.collection("products").findOneAndUpdate({_id: mongodb.ObjectId(req.params.id)},{$set:req.body})
-    
-    await connection.close()
-    
-    res.status(200).json(user)
-    } catch (error) {
-        res.status(500).json({
-            messege:"Somthig went wrong"
-        })
-    }
-    // let userId = req.params.id;
-    // let userIndex = users.findIndex((item) => item.id == userId);
-
-    // if (userIndex != -1) {
-    //     Object.keys(req.body).forEach((item) => {
-    //         users[userIndex][item] = req.body[item]
-    //     });
-    //     res.json({
-    //         messege: "Done"
-    //     });
-    // } else {
-    //     res.json({
-    //         messege: "User not found"
-    //     });
-    // }
 });
 
-app.delete("/user/:id",async function (req, res) {
+app.put("/user/:id", async function (req, res) {
 
     try {
-       
-    const connection = await mongoClient.connect(URL)
-    
-    const db = connection.db(DB)
-    
-    let user = await db.collection("products").findOneAndDelete({_id: mongodb.ObjectId(req.params.id)});
-    
-    await connection.close()
-    
-    res.status(200).json(user)
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let user = await db.collection("users").findOneAndUpdate({ _id: mongodb.ObjectId(req.params.id) }, { $set: req.body })
+
+        await connection.close()
+
+        res.status(200).json(user)
     } catch (error) {
         res.status(500).json({
-            messege:"Somthig went wrong"
+            messege: "Somthig went wrong"
+        })
+    }
+});
+app.put("/product/:id", async function (req, res) {
+
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let user = await db.collection("products").findOneAndUpdate({ _id: mongodb.ObjectId(req.params.id) }, { $set: req.body })
+
+        await connection.close()
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
         })
     }
 
-    // let userId = req.params.id;
-    // let userIndex = users.findIndex((item) => item.id == userId);
-
-    // if (userIndex != -1) {
-    //     users.splice(userIndex, 1);
-    //     res.json({
-    //         messege: "User deleted"
-    //     });
-    // } else {
-    //     res.json({
-    //         messege: "User not found"
-    //     });
-    // }
 });
 
-app.post("/register",async function(req,res){
+app.delete("/user/:id", async function (req, res) {
+
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let user = await db.collection("users").findOneAndDelete({ _id: mongodb.ObjectId(req.params.id) });
+
+        await connection.close()
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        })
+    }
+});
+app.delete("/product/:id", async function (req, res) {
+
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let user = await db.collection("products").findOneAndDelete({ _id: mongodb.ObjectId(req.params.id) });
+
+        await connection.close()
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        })
+    }
 
 });
 
+// app.post("/register", async function (req, res) {
 
-app.listen(process.env.PORT || 3000);
+// });
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
+// app.listen(process.env.PORT || 3000)
